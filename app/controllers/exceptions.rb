eae531c02397c8938bf698fcf588a8c5e91db82b
gmalicki@gmail.com <gmalicki@gmail.com>
2008-11-13 02:27:27 -0800

    mmhrmm

--- app/controllers/exceptions.rb
+++ app/controllers/exceptions.rb
@@ -1,28 +1,28 @@
 class Exceptions < Application
   provides :json                                                
 
-  # handle NotFound exceptions (404)
-  def not_found
-    return standard_error if content_type == :json                
-    render
-  end
-
-  # handle NotAcceptable exceptions (406)
-  def not_acceptable
-    return standard_error if content_type == :json
-    render
-  end
-
-  # handle NotAuthorized exceptions (403)
-  def not_authorized
-    return standard_error if content_type == :json
-    render
-  end
+  # # handle NotFound exceptions (404)
+  # def not_found
+  #   return standard_error if content_type == :json                
+  #   render
+  # end
+  # 
+  # # handle NotAcceptable exceptions (406)
+  # def not_acceptable
+  #   return standard_error if content_type == :json
+  #   render
+  # end
+  # 
+  # # handle NotAuthorized exceptions (403)
+  # def not_authorized
+  #   return standard_error if content_type == :json
+  #   render
+  # end
 
   # Everything else (500)
   def standard_error                                              
     # Re-Raise so we get the pretty merb error document instead.
-    raise request.exceptions.first if content_type == :html       
+    #raise request.exceptions.first if content_type == :html       
 
     @exceptions = request.exceptions
     @show_details = Merb::Config[:exception_details]
