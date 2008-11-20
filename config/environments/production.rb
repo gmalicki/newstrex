83bd94b1ed72d452c831fe2549a09f9cea864827
gmalicki@gmail.com <gmalicki@gmail.com>
2008-11-19 18:25:40 -0800

    layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,15 +13,8 @@ Merb::Config.use { |c|
 
 Merb::Cache.setup do
 
-  # the order that stores are setup is important
-  # faster stores should be setup first
+  Merb::Cache.setup(:tmp_dir, Merb::Cache::FileStore, :dir => Merb.root / :tmp)
 
-  # page cache to the public dir
-  register(:page_store, PageStore[FileStore],
-                    :dir => Merb.root / "public")
-
-
-  # sets up the ordering of stores when attempting to read/write cache entries
-  # register(:default, AdhocStore[:page_store, :action_store])
+  Merb::Cache.setup(:page_store, Merb::Cache::PageStore[Merb::Cache::FileStore], :dir => Merb.root / :public)
 
 end
