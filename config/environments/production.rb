f2ff883ce7f46d17ca3a7b9115d383d3932283df
gmalicki@gmail.com <gmalicki@gmail.com>
2008-11-19 20:04:28 -0800

    hrmm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Merb::Cache.setup do
   # the order that stores are setup is important
   # faster stores should be setup first
   
-  #register(Merb::Cache::FileStore)
+  register(Merb::Cache::FileStore)
 
   # page cache to the public dir
   register(:page_store, Merb::Cache::PageStore[Merb::Cache::FileStore],
