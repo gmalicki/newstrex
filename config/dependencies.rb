e52dc5d7b79d93b596225ec1507f39072fcd9bb1
gmalicki@gmail.com <gmalicki@gmail.com>
2008-11-19 18:22:48 -0800

    layout

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -5,7 +5,7 @@ dm_gems_version   = "0.9.6"
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version  
-require "merb-cache", merb_gems_version   
+require "merb-cache"
 dependency "merb-helpers", merb_gems_version 
 dependency "merb-mailer", merb_gems_version  
 dependency "merb-slices", merb_gems_version  
