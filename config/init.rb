4fee58fa77b834e114aed44e92cdb56c01503607
gmalicki@gmail.com <gmalicki@gmail.com>
2008-11-19 20:12:38 -0800

    hrmm

--- config/init.rb
+++ config/init.rb
@@ -24,8 +24,22 @@ end
  
 Merb::BootLoader.before_app_loads do
   # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+  
+  # hack! remove this when we upgrade.
+  # this is ugly and is only here becuase of this bug: http://wm.lighthouseapp.com/projects/4819/tickets/408-argumenterror-adapter-not-set-default-did-you-forget-to-setup
+  # which has been fixed in merb > 9.2
+  DataMapper.setup(:default, {
+    :adapter => 'mysql',
+    :encoding => 'utf8',
+    :database => 'newstrex_prod',
+    :username => 'gouki',
+    :password => 'Ge0rge', 
+    :host => 'mysql.gouki.dreamhosters.com',
+    :port => 3306
+  })
 end
  
 Merb::BootLoader.after_app_loads do
   # This will get executed after your app's classes have been loaded.
 end
+
