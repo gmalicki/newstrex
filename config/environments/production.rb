62a5225dcf3670f8f4b6047528329c5071a7edbe
gmalicki@gmail.com <gmalicki@gmail.com>
2008-11-19 19:15:58 -0800

    layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,10 @@ Merb::Cache.setup do
   # the order that stores are setup is important
   # faster stores should be setup first
   
-  register(:tmp_cache, FileStore, :dir => "/tmp")
+  register(:tmp_cache, Merb::Cache::FileStore, :dir => "/tmp")
 
   # page cache to the public dir
-  register(:page_store, Merb::Cache::PageStore[FileStore],
+  register(:page_store, Merb::Cache::PageStore[Merb::Cache::FileStore],
                     :dir => Merb.root / "public")
 
 
